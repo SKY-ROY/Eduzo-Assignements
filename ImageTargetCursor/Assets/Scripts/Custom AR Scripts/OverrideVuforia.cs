0b0fa0437334cee88efb0c4164b130f8e0d5aca2
SKY <akash.rm.roy@gmail.com>
2020-10-27 17:10:36 +0530

    Camera window added.

--- ImageTargetCursor/Assets/Scripts/Custom AR Scripts/OverrideVuforia.cs
+++ ImageTargetCursor/Assets/Scripts/Custom AR Scripts/OverrideVuforia.cs
@@ -14,9 +14,11 @@ public class OverrideVuforia : MonoBehaviour
         if(transform.childCount > 1 && !isBGFound)
         {
             cameraBGPlane = transform.GetChild(1).gameObject;
-            cameraBGPlane.SetActive(enableBG);
-
-            isBGFound = true;
+            Debug.Log("Object Found");
+        
+            cameraBGPlane.gameObject.transform.localPosition = new Vector3(-0.625f, 0f, 1.5f);
+            cameraBGPlane.gameObject.transform.localScale = new Vector3(0.125f, 1f, 0.09f);
+            //isBGFound = true;
         }
     }
 }
